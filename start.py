cf79a9b54d43427c8fb7bca414ff7b355751a68f
Ilya Pavlichenko <vighrain@gmail.com>
2022-03-09 17:21:53 +0300

    Added buttons

--- start.py
+++ start.py
@@ -4,13 +4,19 @@ token = '5234059386:AAFWTu3CFmu_HjAx1yTq0qGxlhQHe2EqEyA'
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
-def start(m, res=False):
-        with open('info.txt', encoding="utf-8") as f:
-                info = f.read()
-        bot.send_message(m.chat.id, info)
-
+def start(message):
+        with open('start.txt', encoding="utf-8") as f:
+                start = f.read()
+        markup = telebot.types.InlineKeyboardMarkup()
+        info_button = markup.add(telebot.types.InlineKeyboardButton(text='Инфо', callback_data=1))
+        stats_button = markup.add(telebot.types.InlineKeyboardButton(text='Статистика', callback_data=2))
+        tourism_button = markup.add(telebot.types.InlineKeyboardButton(text='Туризм', callback_data=3))
+        health_button = markup.add(telebot.types.InlineKeyboardButton(text='Медицинская помощь', callback_data=4))
+        bot.send_message(message.chat.id, text=start, reply_markup=markup)
+        
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
 	bot.send_message(message.chat.id, 'Message received: ' + message.text)
 
-bot.infinity_polling()
+if __name__ == '__main__':
+        bot.infinity_polling()
