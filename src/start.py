9649d1a57bf7407f555aab7b5c9d94b3b1f19ba7
Ilya Pavlichenko <vighrain@gmail.com>
2022-05-31 13:46:27 +0300

    Изменения в документации start

--- src/start.py
+++ src/start.py
@@ -6,19 +6,29 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-	"""Вывод на экран начального меню бота"""
-	with open('../data/start.txt', encoding="utf-8") as f:
-		start = f.read()
-	markup = telebot.types.InlineKeyboardMarkup()
-	info_button = markup.add(telebot.types.InlineKeyboardButton(text='Инфо', callback_data=1))
-	stats_button = markup.add(telebot.types.InlineKeyboardButton(text='Статистика', callback_data=2))
-	tourism_button = markup.add(telebot.types.InlineKeyboardButton(text='Туризм', callback_data=3))
-	health_button = markup.add(telebot.types.InlineKeyboardButton(text='Медицинская помощь', callback_data=4))
-	bot.send_message(message.chat.id, text=start, reply_markup=markup)
+        """
+        Вывод на экран начального меню бота
+
+        Выводится приветствие из /data/start.txt
+        и отображаются 5 кнопок для взаимодействия с ботом.
+        
+        """
+        with open('../data/start.txt', encoding="utf-8") as f:
+                start = f.read()
+        markup = telebot.types.InlineKeyboardMarkup()
+        info_button = markup.add(telebot.types.InlineKeyboardButton(text='Инфо', callback_data=1))
+        stats_button = markup.add(telebot.types.InlineKeyboardButton(text='Статистика', callback_data=2))
+        tourism_button = markup.add(telebot.types.InlineKeyboardButton(text='Туризм', callback_data=3))
+        health_button = markup.add(telebot.types.InlineKeyboardButton(text='Медицинская помощь', callback_data=4))
+        bot.send_message(message.chat.id, text=start, reply_markup=markup)
         
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
-	bot.send_message(message.chat.id, 'Message received: ' + message.text)
+        """
+        Отправляет введенное сообщение (тестовая функция, будет удалена)
+
+        """
+        bot.send_message(message.chat.id, 'Message received: ' + message.text)
 
 if __name__ == '__main__':
         bot.infinity_polling()
